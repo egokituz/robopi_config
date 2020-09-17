048c2f7566b3948346cb7b0bdd59d76fec4ca62a
Xabier Gardeazabal <xabier.gardeazabal@ehu.eus>
2020-09-17 13:48:18 +0200

    Edited prueba.sh

--- prueba.sh
+++ prueba.sh
@@ -7,16 +7,36 @@ echo "Script de configuración para robopi"
 
 # Am i Root user?
 if [ $(id -u) -eq 0 ]; then
-	## Borrar usuarios anteriores
+	## Borrar usuarios "grupoXY" existentes
 
-	users=$(grep -io "grupo[0-9]*" /etc/passwd | sort | uniq) # Obtener la lista de usuarios que coincidan con grupoXY"
+	users=$(grep -io "grupo[0-9]*" /etc/passwd | sort | uniq) # Obtener usuarios que coincidan con grupoXY
 	
 	for grupo in $users
 	do
-		echo "Usuario $grupo"
+		echo "Borrando usuario $grupo ..."
 		$(userdel -f -r $grupo)
 		rm /home/$grupo
 	done
+
+
+	# Enable SSH
+	sudo systemctl enable ssh
+	sudo systemctl start ssh
+
+
+	# Rename /etc/hostname
+
+
+	# Rename /etc/hosts
+
+
+	# Configure /etc/dhcpcd.conf
+
+
+	# Configure /etc/wpa_supplicant/wpa_supplicant.conf
+
+
+
 else
 	echo "Only root may add a user to the system."
 	exit 2
